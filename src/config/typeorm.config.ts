3e94ebb953ce1c2df1ab31f03ed924e9631c9fc3
alexeiidonetskiy <isalfetkin@gmail.com>
2020-04-23 20:34:33 +0300

    Update env variables, prepared the environment for AWS serving

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -9,7 +9,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   port: process.env.RDS_PORT || dbConfig.port,
   username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
-  database: process.env.RDS_DATABASE || dbConfig.database,
+  database: process.env.RDS_DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize,
 };
